d1e50135513daa3c0273d345a811f5297a3da829
dewashington33 <dewashington@hotmail.com>
2024-03-12 00:20:42 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from 'react';
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [localAmount, setLocalAmount] = useState(amount);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [convertedAmount, setConvertedAmount] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    setLocalAmount(amount);
+  }, [amount]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [localAmount]); 
+
+  const handleAmountChange = (e) => {
+    setLocalAmount(Number(e.target.value));
+  };
+
+  const handleBlur = () => {
+    setAmount(localAmount);
+  };
+
+useEffect(
+  function() {
+    async function convert() {
+      setIsLoading(true);
+      const response = await fetch(
+       `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`
+      //  `https://www.exchangerate-api.com/docs/free/${fromCurrency}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setConvertedAmount(data.rates[toCurrency] * amount);
+      setIsLoading(false);
+}
+
+if (fromCurrency === toCurrency) return setConvertedAmount(amount);
+convert();
+  } ,
+  [fromCurrency, toCurrency, amount]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        type="text" 
+        value={localAmount}
+        onChange={handleAmountChange}
+        onBlur={handleBlur}
+        disabled={isLoading}
+        ref={inputRef}
+      />
+      <select name="fromCurrency" id="fromCurrency"
+       value={fromCurrency}
+       onChange={(e) => setFromCurrency(e.target.value)}
+       disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">CHF</option>
+      </select>
+      <select name="toCurrency" id="toCurrency"
+      value={toCurrency}
+      onChange={(e) => setToCurrency(e.target.value)}
+      disabled={isLoading}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">CHF</option>
+      </select>
+      <p>{convertedAmount} {toCurrency}</p>
     </div>
   );
 }
